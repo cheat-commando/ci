8ca77008b3e95a50a23543d622f124468e8183c1
Carston Work <carston.work@gmail.com>
2021-08-30 15:34:29 -0600

    Added new test

--- test.js
+++ test.js
@@ -2,4 +2,5 @@ const getGreeting = require('./project');
 
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
+  expect(getGreeting('Judy')).toBe("Hi Judy");
 });
