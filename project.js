ff97ac868947ef356f3025840be92f1f457bd928
Carston Work <carston.work@gmail.com>
2021-08-30 15:41:56 -0600

    Added more tests

--- project.js
+++ project.js
@@ -2,4 +2,11 @@ function getGreeting(name){
     return "Hi " + name;
 };
 
-module.exports = getGreeting;
+function displayAge(age) {
+    const plural = age === 1 ? '' : 's'
+    return `You are ${age} year${plural} old.`
+}
+module.exports = {
+    getGreeting,
+    displayAge
+}
--- test.js
+++ test.js
@@ -1,6 +1,11 @@
-const getGreeting = require('./project');
+const { getGreeting, displayAge } = require('./project');
 
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
   expect(getGreeting('Judy')).toBe("Hi Judy");
+});
+
+test('can display your age', () => {
+  expect(displayAge(15)).toBe('You are 15 years old.')
+  expect(displayAge(1)).toBe('You are 1 year old.')
 });
